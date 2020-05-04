621b4cabf99f3d8094f082ff6cd099f4efaa4eb8
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-04 18:17:35 -0300

    configured multer and route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
 
 router.get("/", (req, res) => {
+  return res.json({ hello: "World" });
+});
+router.post("/posts", multer(multerConfig).single("file"), (req, res) => {
+  console.log(req.file);
   return res.json({ hello: "World" });
 });
 
