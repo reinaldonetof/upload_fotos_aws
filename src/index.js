4bb952a3f04eab2986988202e0f725bc6967da79
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-04 18:04:24 -0300

    gitiignore .env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
 
 app.use(require("./routes"));
 
