4f4724879241406ee54a2ac48fa8b8c6ad91a573
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-05 00:16:17 -0300

    atualizando como pegar process.env

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+const dotenv = require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/upload", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 app.use(require("./routes"));
 
